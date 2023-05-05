7a3878937ec97c73ebc5744f216b19ae18996148
Cyprian Tinashe Aarons <cyprianaarons263@gmail.com>
2023-05-06 00:06:30 +0200

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,12 +5,20 @@ import { Message } from '@/types/chat';
 import ReactMarkdown from 'react-markdown';
 import LoadingDots from '@/components/ui/LoadingDots';
 import { Document } from 'langchain/document';
+const API_KEY = "bW9uZXl3YW5ndUBnbWFpbC5jb20:8baQJm9WZQFyKGtrYtnQo"
+const API_URL = "https://api.d-id.com"
 
 export default function Home() {
+  //DID Video
+  const talkVideo = useRef<HTMLVideoElement>(null);
+  const [videoLoading, setVideoLoading] = useState<boolean>(false);
+  const [streamId, setStreamId] = useState<string | null>(null);
+  const [connected, setConnected] = useState<boolean>(false);
+  const [peerConnection, setPeerConnection] = useState<RTCPeerConnection | null>(null);
+  //chat
   const [query, setQuery] = useState<string>('');
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
-  const [videoID, setVideoID] = useState<string | null>(null);
   const [audioURL, setAudioURL] = useState<string | null>(null);
   const [messageState, setMessageState] = useState<{
     messages: Message[];
@@ -32,7 +40,7 @@ export default function Home() {
       text: string;
       sourceDocuments?: Document[];
     };
- } | null>(null);
+  } | null>(null);
 
   const { messages, history } = messageState;
 
@@ -88,15 +96,25 @@ export default function Home() {
         setError(data.error);
       } else {
         handleAudio(data.text);
-        setNewMessage({
-          question: question, data: data
-        });
+        setMessageState((state) => ({
+          ...state,
+          messages: [
+            ...state.messages,
+            {
+              type: 'apiMessage',
+              message: data.text,
+              sourceDocs: data.sourceDocuments,
+            },
+          ],
+          history: [...state.history, [question, data.text]],
+        }));
+        setLoading(false);
       }
 
       //scroll to bottom
       messageListRef.current?.scrollTo(0, messageListRef.current.scrollHeight);
     } catch (error) {
-      setLoading(false);
+     
       setError('An error occurred while fetching the data. Please try again.');
       console.log('error', error);
     }
@@ -120,7 +138,7 @@ export default function Home() {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-          text:text,
+          text: text,
         }),
       }).then(
         async (response) => {
@@ -131,145 +149,335 @@ export default function Home() {
           setAudioURL(data?.audioURL);
         }
       )
-      
+
     } catch (error) {
       console.log('error', error);
     }
   }
 
+  //Did Video 
+  //Connect to rpc server
+  let RTCPeerConnection: typeof window.RTCPeerConnection;
+
+  if (typeof window !== 'undefined') {
+    // @ts-ignore
+    RTCPeerConnection = window.RTCPeerConnection || window.webkitRTCPeerConnection || window.mozRTCPeerConnection;
+  }
+
+  let sId: any;
+  let sessionId: any;
+  let sessionClientAnswer: any;
+
+  const connect = async () => {
+    if (peerConnection && peerConnection.connectionState === 'connected') {
+      return;
+    }
+
+    stopAllStreams();
+    closePC();
+
+    const sessionResponse = await fetch(`${API_URL}/talks/streams`, {
+      method: 'POST',
+      headers: { 'Authorization': `Basic ${API_KEY}`, 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        source_url: "https://res.cloudinary.com/dqzpz4w3l/image/upload/v1683232752/bushiri-transformed_znyzq1.png"
+      }),
+    });
+
+    const { id: newStreamId, offer, ice_servers: iceServers, session_id: newSessionId } = await sessionResponse.json()
+    setStreamId(newStreamId);
+    sId = newStreamId;
+    sessionId = newSessionId;
+
+
+
+    try {
+      sessionClientAnswer = await createPeerConnection(offer, iceServers);
+      setConnected(true);
+    } catch (e) {
+      console.log('error during streaming setup', e);
+      stopAllStreams();
+      closePC();
+      return;
+    }
+
+    const sdpResponse = await fetch(`${API_URL}/talks/streams/${sId}/sdp`,
+      {
+        method: 'POST',
+        headers: { Authorization: `Basic ${API_KEY}`, 'Content-Type': 'application/json' },
+        body: JSON.stringify({ answer: sessionClientAnswer, session_id: sessionId })
+      });
+  }
+  
+  const createPeerConnection = async (offer: any, iceServers: any) => {
+    let pc = new RTCPeerConnection({ iceServers });
+    if (!peerConnection) {
+
+      //@ts-ignore
+      pc?.addEventListener('icecandidate', onIceCandidate, true);
+      //@ts-ignore
+      pc?.addEventListener('track', onTrack, true);
+
+      setPeerConnection(pc);
+    }
+
+    await pc?.setRemoteDescription(offer);
+    console.log('set remote sdp OK');
+
+    const sessionClientAnswer = await pc?.createAnswer();
+    console.log('create local sdp OK');
+
+    await pc?.setLocalDescription(sessionClientAnswer);
+    console.log('set local sdp OK');
+
+    console.log('peer connection OK', sessionClientAnswer)
+
+    return sessionClientAnswer;
+
+  }
+
+  const talk = async () => {
+
+    console.log("Peer Connection", peerConnection); // Add this line to debug
+    if (!peerConnection) {
+      console.log("Peer Connection is undefined");
+      return;
+    }
+
+    if (peerConnection?.signalingState === 'stable' || peerConnection?.iceConnectionState === 'connected') {
+      await fetch(
+        `${API_URL}/talks/streams/${streamId}`,
+        {
+          method: "POST",
+          headers: {
+            Authorization: `Basic ${API_KEY}`,
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            script: {
+              type: "audio",
+              audio_url:
+                audioURL,
+            },
+            driver_url: "bank://lively/",
+            config: {
+              stitch: true,
+            },
+            session_id: sessionId,
+          }),
+        }
+      ).then(async (response) => {
+        setLoading(true);
+        const data = await response.json();
+        console.log(data);
+      }).catch((error) => {
+        console.log(error);
+      })
+
+    }
+  }
+
+  const onIceCandidate = (event: any) => {
+    console.log('onIceCandidate', event);
+    if (event.candidate) {
+      const { candidate, sdpMid, sdpMLineIndex } = event.candidate;
+
+      fetch(`${API_URL}/talks/streams/${sId}/ice`,
+        {
+          method: 'POST',
+          headers: { Authorization: `Basic ${API_KEY}`, 'Content-Type': 'application/json' },
+          body: JSON.stringify({ candidate, sdpMid, sdpMLineIndex, session_id: sessionId })
+        });
+    }
+  }
+
+
+  const onTrack = (event: any) => {
+    const remoteStream = event.streams[0];
+    setVideoElement(remoteStream);
+  }
+
+  const setVideoElement = (stream: any) => {
+    if (!stream || !talkVideo || !talkVideo.current) return;
+
+    talkVideo.current.srcObject = stream;
+
+    // safari hotfix
+
+    if (talkVideo.current.paused) {
+
+      talkVideo.current.play().then(_ => { }).catch(e => { });
+    }
+  }
+
+  const stopAllStreams = () => {
+    if (!talkVideo || !talkVideo.current) return;
+    if (talkVideo.current.srcObject) {
+      console.log('stopping video streams');
+       //@ts-ignore
+      talkVideo.current.srcObject.getTracks().forEach(track => track.stop());
+
+      talkVideo.current.srcObject = null;
+    }
+  }
+
+  const closePC = () => {
+    if (!peerConnection) return;
+    console.log('stopping peer connection');
+    peerConnection.close();
+    peerConnection.removeEventListener('icecandidate', onIceCandidate, true);
+    console.log('stopped peer connection');
+    if (peerConnection === peerConnection) {
+      setPeerConnection(null);
+    }
+  }
+
   useEffect(() => {
     if (audioURL) {
-      setLoading(false);
-      //@ts-ignore
-      setMessageState((state) => ({
-        ...state,
-        messages: [
-          ...state.messages,
-          {
-            type: 'apiMessage',
-            message: newMessage?.data?.text,
-            sourceDocs: newMessage?.data?.sourceDocuments,
-          },
-        ],
-        history: [...state.history, [newMessage?.question, newMessage?.data?.text]],
-
-      }));
-      //play audio
-      const audio = new Audio(audioURL);
-      audio.play();
+      console.log("Audio URL", peerConnection);
+      talk();
     }
   }, [audioURL]);
+
+  useEffect(() => {
+    if (peerConnection) {
+      console.log("Peer Connection", peerConnection);
+      console.log("Talk Video", sessionClientAnswer);
+    }
+  },
+    [peerConnection, sessionClientAnswer]);
+
 
 
   return (
     <>
       <Layout>
-        <div className="flex flex-col mx-auto ">
-          <div className='flex justify-center m-4'>
-            <div className='relative isolate'>
-              <div className="relative  group">
-                <div className="absolute transition duration-1000 rounded-full opacity-25 -inset-2 bg-gradient-to-r from-purple-600 to-pink-600 blur group-hover:opacity-50 group-hover:duration-200"></div>
-                <div className='relative  z-10 sm:w-96 sm:h-96 w-64 h-64 bg-gray-100 rounded-full'>
-                  <video className='absolute object-cover sm:w-96 sm:h-96 w-64 h-64 rounded-full z-12' autoPlay
-                    loop
-                    muted
-                   playsInline src={
-                      videoID ? `${videoID}` : '/bur.mp4'
-                    }></video>
-                    
+        {connected ? (
+          <div className="flex flex-col mx-auto ">
+            <div className='flex justify-center m-4'>
+              <div className='relative isolate'>
+                <div className="relative  group">
+                  <div className="absolute transition duration-1000 rounded-full opacity-25 -inset-2 bg-gradient-to-r from-purple-600 to-pink-600 blur group-hover:opacity-50 group-hover:duration-200"></div>
+                  <div className='relative  z-10 sm:w-96 sm:h-96 w-64 h-64 bg-gray-100 rounded-full'>
+                    <video
+                      className='absolute object-cover sm:w-96 sm:h-96 w-64 h-64 rounded-full z-12' autoPlay
+                      playsInline
+
+                      loop
+                      src="./bur.mp4"
+                    ></video>
+                  </div>
                 </div>
               </div>
+            </div>
+            <video
+              className='absolute object-cover sm:w-96 sm:h-96 w-64 h-64 rounded-full z-12' autoPlay
+              playsInline
+              ref={talkVideo}
+              loop
+
+            ></video>
+
+            <main className={styles.main}>
+              <div className={styles.cloud}>
+                <div ref={messageListRef} className={styles.messagelist}>
+                  {messages.map((message, index) => {
+                    let className;
+                    return (
+                      <>
+                        <div key={`chatMessage-${index}`} className={className}>
+
+                          <div className={styles.markdownanswer}>
+                            {message.type === 'apiMessage' && (
+                              <div className="my-2 text-left place-self-start" >
+                                <ReactMarkdown className='p-5 sm:text-xl text-sm font-light rounded-tl-none ' linkTarget="_blank">
+                                  {message.message}
+                                </ReactMarkdown>
+                              </div>
+                            )}
+                            {message.type === 'userMessage' && (
+                              <div className="my-2 text-right place-self-start">
+                                <ReactMarkdown className='p-5 sm:text-xl text-sm font-light text-purple-500 rounded-tr-none rounded-2xl' linkTarget="_blank">
+                                  {message.message}
+                                </ReactMarkdown>
+                              </div>
+                            )}
+                          </div>
+                        </div>
+
+                      </>
+                    );
+                  })}
+                </div>
+              </div>
+
+              <div className="absolute bottom-0 left-0 w-full pt-6 border-transparent bg-gradient-to-b from-transparent via-white to-white  md:pt-2">
+                <div className={styles.center}>
+                  <div className={styles.cloudform}>
+
+                    <form onSubmit={handleSubmit}>
+                      <textarea
+                        disabled={loading}
+                        onKeyDown={handleEnter}
+                        ref={textAreaRef}
+                        autoFocus={false}
+                        rows={1}
+                        maxLength={512}
+                        id="userInput"
+                        name="userInput"
+                        placeholder={
+                          loading
+                            ? 'Waiting for response...'
+                            : 'Hi there, how can I help?'
+                        }
+                        value={query}
+                        onChange={(e) => setQuery(e.target.value)}
+                        className={styles.textarea}
+                      />
+                      <button
+                        type="submit"
+                        disabled={loading}
+                        className={styles.generatebutton}
+                      >
+                        {loading ? (
+                          <div className={styles.loadingwheel}>
+                            <LoadingDots color="#000" />
+                          </div>
+                        ) : (
+                          // Send icon SVG in input field
+                          <svg
+                            viewBox="0 0 20 20"
+                            className={styles.svgicon}
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"></path>
+                          </svg>
+                        )}
+                      </button>
+                    </form>
+                  </div>
+                </div>
+              </div>
+
+              {error && (
+                <div className="p-4 border border-red-400 rounded-md">
+                  <p className="text-red-500">{error}</p>
+                </div>
+              )}
+            </main>
+          </div>
+        ) : (
+          <div className="flex min-h-screen ">
+            <div className='m-auto'>
+              <button
+                onClick={connect}
+                className='bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-8 rounded-full'
+              >
+                Start Chat
+              </button>
             </div>
           </div>
-
-          <main className={styles.main}>
-            <div className={styles.cloud}>
-              <div ref={messageListRef} className={styles.messagelist}>
-                {messages.map((message, index) => {
-                  let className;
-                  return (
-                    <>
-                      <div key={`chatMessage-${index}`} className={className}>
-
-                        <div className={styles.markdownanswer}>
-                          {message.type === 'apiMessage'  && (
-                            <div className="my-2 text-left place-self-start" >
-                              <ReactMarkdown className='p-5 sm:text-xl text-sm font-light rounded-tl-none ' linkTarget="_blank">
-                                {message.message}
-                              </ReactMarkdown>
-                            </div>
-                          )}
-                          {message.type === 'userMessage' && (
-                            <div className="my-2 text-right place-self-start">
-                              <ReactMarkdown className='p-5 sm:text-xl text-sm font-light text-purple-500 rounded-tr-none rounded-2xl' linkTarget="_blank">
-                                {message.message}
-                              </ReactMarkdown>
-                            </div>
-                          )}
-                        </div>
-                      </div>
-
-                    </>
-                  );
-                })}
-              </div>
-            </div>
-
-            <div className="absolute bottom-0 left-0 w-full pt-6 border-transparent bg-gradient-to-b from-transparent via-white to-white  md:pt-2">
-              <div className={styles.center}>
-                <div className={styles.cloudform}>
-     
-                  <form onSubmit={handleSubmit}>
-                    <textarea
-                      disabled={loading}
-                      onKeyDown={handleEnter}
-                      ref={textAreaRef}
-                      autoFocus={false}
-                      rows={1}
-                      maxLength={512}
-                      id="userInput"
-                      name="userInput"
-                      placeholder={
-                        loading
-                          ? 'Waiting for response...'
-                          : 'Hi there, how can I help?'
-                      }
-                      value={query}
-                      onChange={(e) => setQuery(e.target.value)}
-                      className={styles.textarea}
-                    />
-                    <button
-                      type="submit"
-                      disabled={loading}
-                      className={styles.generatebutton}
-                    >
-                      {loading ? (
-                        <div className={styles.loadingwheel}>
-                          <LoadingDots color="#000" />
-                        </div>
-                      ) : (
-                        // Send icon SVG in input field
-                        <svg
-                          viewBox="0 0 20 20"
-                          className={styles.svgicon}
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"></path>
-                        </svg>
-                      )}
-                    </button>
-                  </form>
-                </div>
-              </div>
-            </div>
-            
-            {error && (
-              <div className="p-4 border border-red-400 rounded-md">
-                <p className="text-red-500">{error}</p>
-              </div>
-            )}
-          </main>
-        </div>
+        )}
 
       </Layout>
     </>
